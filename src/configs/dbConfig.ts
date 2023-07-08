b269cd2cc13db17e48deada22ca815696ada4677
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-07-08 17:24:47 +0530

    update the env file

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.HOST,
-  port: parseInt(process.env.PORT || '3306'),
-  username: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['src/entity/*.js'],
   logging: true,
   synchronize: true,
