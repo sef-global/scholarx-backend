5c1588c7a978feff5d172f4b1ccca6bd914adf8b
Shrenik Deep <s.shrenikdeep@gmail.com>
2023-08-02 10:19:07 +0530

    removing unnecessary comments

--- src/routes/profile.route.test.ts
+++ src/routes/profile.route.test.ts
@@ -3,38 +3,32 @@ import supertest from 'supertest';
 
 describe('profile', () => {
   describe('Get profile route', () => {
-    let accessToken: string; // Declare the access token as a variable accessible across tests
+    let accessToken: string;
 
     beforeAll(async () => {
-      // Create a test user (you can use a testing database or mock the user creation)
       const testUser = {
         email: 'test@gmail.com',
         password: '123',
       };
 
-      // Log in the test user to get the access token
       const response = await supertest(server)
         .post('/api/auth/login')
         .send(testUser)
         .expect(200);
 
-      // Extract the access token from the response body
       accessToken = response.body.token;
     });
 
-    // Test if api returns 401 Unauthorised when no access token is passed
     it('should return a 401 without a valid access token', async () => {
       await supertest(server).get('/api/me/profile').expect(401);
     });
 
     it('should return a 200 with a user profile object', async () => {
-      // Make the request with the valid access token (expect 200 OK)
       const response = await supertest(server)
         .get('/api/me/profile')
         .set('Authorization', `Bearer ${accessToken}`)
         .expect(200);
 
-      // Verify the response contains the expected keys
       expect(response.body).toHaveProperty('created_at');
       expect(response.body).toHaveProperty('updated_at');
       expect(response.body).toHaveProperty('primary_email');
