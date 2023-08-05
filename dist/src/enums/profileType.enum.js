51e415c4d91996cd2552760f83555b5cd563b4fc
anjula-sack <anjulashanaka@gmail.com>
2023-08-06 00:02:46 +0530

    Remove build files

--- dist/src/enums/profileType.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProfileTypes = void 0;
-var ProfileTypes;
-(function (ProfileTypes) {
-    ProfileTypes["DEFAULT"] = "default";
-    ProfileTypes["ADMIN"] = "admin";
-})(ProfileTypes || (exports.ProfileTypes = ProfileTypes = {}));
--- dist/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
-(0, app_1.default)().then(() => {
-    console.log("Server started!");
-}).catch((err) => { console.log("Something went wrong!", err); });
