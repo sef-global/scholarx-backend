b30bc86416601708b30ec93ea5f56ae7a6f86cf9
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-08-13 00:49:07 +0530

    Fix formatting

--- src/middleware/mentor.middleware.ts
+++ src/middleware/mentor.middleware.ts
@@ -1,16 +1,16 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express'
 
 export const validateMentorData = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const mentorData = req.body;
+  const mentorData = req.body
 
   if (!Array.isArray(mentorData) || mentorData.length === 0) {
     return res.status(400).json({
-      error: 'Mentors data must be an array with at least one element',
-    });
+      error: 'Mentors data must be an array with at least one element'
+    })
   }
 
   for (const mentor of mentorData) {
@@ -22,8 +22,8 @@ export const validateMentorData = (
       typeof mentor.answers !== 'string' ||
       !mentor.answers.trim()
     ) {
-      return res.status(400).json({ error: 'Invalid mentor data format' });
+      return res.status(400).json({ error: 'Invalid mentor data format' })
     }
   }
-  next();
-};
+  next()
+}
--- src/services/mentor.service.ts
+++ src/services/mentor.service.ts
@@ -1,36 +1,39 @@
-import internal from 'stream';
 import { dataSource } from '../configs/dbConfig'
-import Mentor from '../entities/mentor.entity';
-import Profile from '../entities/profile.entity';
-import { ApplicationStatus } from '../enums';
-import Category from '../entities/category.entity';
+import Mentor from '../entities/mentor.entity'
+import Profile from '../entities/profile.entity'
+import { ApplicationStatus } from '../enums'
+import Category from '../entities/category.entity'
 
 export const createMentor = async (req: any) => {
+  const userId = req.user.uuid
+  const userApplication = req.body
 
-  const userId = req.user.uuid;
-  const userApplication = req.body;
-  
-  
   try {
-    const profileRepository = dataSource.getRepository(Profile);
-    const userProfile = await profileRepository.findOne({ where: { uuid: userId } });
+    const profileRepository = dataSource.getRepository(Profile)
+    const userProfile = await profileRepository.findOne({
+      where: { uuid: userId }
+    })
 
     if (!userProfile) {
-      throw new Error('User profile not found');
+      throw new Error('User profile not found')
     }
 
-    const mentorRepository = dataSource.getRepository(Mentor);
-    const existingMentor = await mentorRepository.findOne({ where: { uuid: userId } });
+    const mentorRepository = dataSource.getRepository(Mentor)
+    const existingMentor = await mentorRepository.findOne({
+      where: { uuid: userId }
+    })
 
     if (existingMentor) {
       if (existingMentor.state === ApplicationStatus.PENDING) {
-        return 'Application pending';
+        return 'Application pending'
       } else {
-        const categoryRepository = dataSource.getRepository(Category);
-        const category = await categoryRepository.findOne({ where: {uuid: "60b5b847-99a2-4e47-b35b-81b4284311dd"}});
+        const categoryRepository = dataSource.getRepository(Category)
+        const category = await categoryRepository.findOne({
+          where: { uuid: '60b5b847-99a2-4e47-b35b-81b4284311dd' }
+        })
 
         if (!category) {
-          throw new Error('User profile not found');
+          throw new Error('User profile not found')
         }
 
         const newMentor = new Mentor(
@@ -40,16 +43,18 @@ export const createMentor = async (req: any) => {
           true,
           userProfile,
           []
-        );
-        await mentorRepository.save(newMentor);
-        return 'Mentor added';
+        )
+        await mentorRepository.save(newMentor)
+        return 'Mentor added'
       }
     } else {
-      const categoryRepository = dataSource.getRepository(Category);
-      const category = await categoryRepository.findOne({ where: {uuid: "60b5b847-99a2-4e47-b35b-81b4284311dd"}});
+      const categoryRepository = dataSource.getRepository(Category)
+      const category = await categoryRepository.findOne({
+        where: { uuid: '60b5b847-99a2-4e47-b35b-81b4284311dd' }
+      })
 
       if (!category) {
-        throw new Error('User profile not found');
+        throw new Error('User profile not found')
       }
 
       const newMentor = new Mentor(
@@ -59,12 +64,12 @@ export const createMentor = async (req: any) => {
         true,
         userProfile,
         []
-      );
-      await mentorRepository.save(newMentor);
-      return 'Mentor added';
+      )
+      await mentorRepository.save(newMentor)
+      return 'Mentor added'
     }
   } catch (err) {
-    console.error('Error creating mentor', err);
-    throw new Error('Error creating mentor');
+    console.error('Error creating mentor', err)
+    throw new Error('Error creating mentor')
   }
-};
+}
