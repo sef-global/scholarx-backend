d8ad437703d85089c103d80acfa7c799ef705dda
Anjula Shanaka <anjulashanaka@gmail.com>
2023-08-13 23:03:30 +0530

    Improve the entities and the HTTP status codes (#56)

--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -1,44 +1,14 @@
-import {
-  BeforeInsert,
-  BeforeUpdate,
-  Column,
-  Entity,
-  PrimaryGeneratedColumn
-} from 'typeorm'
-import { v4 as uuidv4 } from 'uuid'
+import { Column, Entity } from 'typeorm'
+import BaseEntity from './baseEntity'
 
 @Entity('category')
-class Category {
-  @PrimaryGeneratedColumn('uuid')
-  uuid!: string
-
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  created_at: Date | undefined
-
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  updated_at: Date | undefined
-
+class Category extends BaseEntity {
   @Column({ type: 'varchar', length: 255 })
   category: string
 
   constructor(category: string) {
+    super()
     this.category = category
-  }
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  updateTimestamps(): void {
-    this.updated_at = new Date()
-    if (!this.uuid) {
-      this.created_at = new Date()
-    }
-  }
-
-  @BeforeInsert()
-  async generateUuid(): Promise<void> {
-    if (!this.uuid) {
-      this.uuid = uuidv4()
-    }
   }
 }
 
--- src/entities/email.entity.ts
+++ src/entities/email.entity.ts
@@ -1,18 +1,9 @@
-import {
-  BeforeInsert,
-  BeforeUpdate,
-  Column,
-  Entity,
-  PrimaryGeneratedColumn
-} from 'typeorm'
+import { Column, Entity } from 'typeorm'
 import { EmailStatusTypes } from '../enums'
-import { v4 as uuidv4 } from 'uuid'
+import BaseEntity from './baseEntity'
 
 @Entity('email')
-class Email {
-  @PrimaryGeneratedColumn('uuid')
-  uuid!: string
-
+class Email extends BaseEntity {
   @Column({ type: 'varchar', length: 255 })
   recipient: string
 
@@ -25,38 +16,17 @@ class Email {
   @Column({ type: 'enum', enum: EmailStatusTypes })
   state: EmailStatusTypes
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  created_at: Date | undefined
-
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  updated_at: Date | undefined
-
   constructor(
     recipient: string,
     subject: string,
     content: string,
     state: EmailStatusTypes
   ) {
+    super()
     this.recipient = recipient
     this.subject = subject
     this.content = content
     this.state = state
-  }
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  updateTimestamps(): void {
-    this.updated_at = new Date()
-    if (!this.uuid) {
-      this.created_at = new Date()
-    }
-  }
-
-  @BeforeInsert()
-  async generateUuid(): Promise<void> {
-    if (!this.uuid) {
-      this.uuid = uuidv4()
-    }
   }
 }
 
