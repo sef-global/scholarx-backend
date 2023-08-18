932e287d2467712507af62f5dc488025515db4d6
Shrenik Deep <s.shrenikdeep@gmail.com>
2023-08-18 17:50:57 +0530

    Issue fixes

--- src/controllers/admin/user.controller.ts
+++ src/controllers/admin/user.controller.ts
@@ -4,15 +4,14 @@ import { getAllUsers } from '../../services/admin.service'
 export const getAllUsersHandler = async (
   req: Request,
   res: Response
-): Promise<void> => {
+): Promise<any> => {
   try {
     const users = await getAllUsers(req)
-    console.log(users)
-    if (users && users.length != 0) {
-      res.status(404).json({ message: 'No users available' })
+    if (users?.length === 0) {
+      return res.status(404).json({ message: 'No users available' })
+    } else {
+      return res.status(200).json({ profiles: users })
     }
-
-    res.status(200).json(users)
   } catch (err) {
     console.error('Error executing query', err)
     res.status(500).json({ error: err })
