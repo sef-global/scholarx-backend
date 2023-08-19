990c846ca9cdbc3e63759327b1d46943675dc149
Shrenik Deep <s.shrenikdeep@gmail.com>
2023-08-19 12:32:02 +0530

    Unit Test updates

--- src/routes/admin/admin.route.test.ts
+++ src/routes/admin/admin.route.test.ts
@@ -1,22 +1,40 @@
+import { startServer } from '../../app'
 import type { Express } from 'express'
 import supertest from 'supertest'
 import Profile from '../../entities/profile.entity'
 
+const port = Math.floor(Math.random() * (9999 - 3000 + 1)) + 3000
+
+const randomString = Math.random().toString(36)
 let server: Express
-let accessToken: string
+let agent: supertest.SuperAgentTest
 
 describe('Get all users route', () => {
+  beforeAll(async () => {
+    server = await startServer(port)
+    agent = supertest.agent(server)
+
+    const testUser = {
+      email: `test${randomString}@gmail.com`,
+      password: '123'
+    }
+
+    await supertest(server)
+      .post('/api/auth/register')
+      .send(testUser)
+      .expect(201)
+
+    await agent.post('/api/auth/login').send(testUser).expect(200)
+  }, 5000)
+
   it('should return a 401 when a valid access token is not provided', async () => {
     await supertest(server).get('/api/admin/users').expect(401)
   })
 
   it('should return a 200 with all users from the DB', async () => {
-    const response = await supertest(server)
-      .get('/api/me/profile')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .expect(200)
+    const response = await agent.get('/api/admin/users').expect(200)
 
-    const userProfiles = response.body
+    const userProfiles = response.body.profiles
 
     userProfiles.forEach((userProfile: Partial<Profile>) => {
       expect(userProfile).toHaveProperty('created_at')
