c95c2b443757512afc5f4802f7a1fe62e7b4e0ad
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-08-23 22:56:01 +0530

    Fix issue with category id

--- src/routes/mentor/mentor.route.test.ts
+++ src/routes/mentor/mentor.route.test.ts
@@ -4,12 +4,14 @@ import supertest from 'supertest'
 import { dataSource } from '../../configs/dbConfig'
 import { mentorApplicationInfo } from '../../../mocks'
 import { v4 as uuidv4 } from 'uuid'
+import Category from '../../entities/category.entity'
 
 const randomString = Math.random().toString(36)
 const port = Math.floor(Math.random() * (9999 - 3000 + 1)) + 3000
 
 let server: Express
 let agent: supertest.SuperAgentTest
+let savedCategory: Category
 
 describe('Mentor application', () => {
   beforeAll(async () => {
@@ -27,13 +29,17 @@ describe('Mentor application', () => {
       .expect(201)
 
     await agent.post('/api/auth/login').send(testUser).expect(200)
+
+    const categoryRepository = dataSource.getRepository(Category)
+    const newCategory = new Category('Random Category', [])
+    savedCategory = await categoryRepository.save(newCategory)
   }, 5000)
 
   describe('Apply as a mentor route', () => {
     it('should return a 200 with a mentor object and the message', async () => {
       const response = await agent
         .post('/api/mentors')
-        .send(mentorApplicationInfo)
+        .send({ ...mentorApplicationInfo, categoryId: savedCategory.uuid })
         .expect(200)
 
       expect(response.body).toHaveProperty('mentor')
@@ -43,7 +49,7 @@ describe('Mentor application', () => {
     it('should return a 409 when the user is already a mentor or has an pending invitation', async () => {
       const response = await supertest(server)
         .post('/api/mentors')
-        .send(mentorApplicationInfo)
+        .send({ ...mentorApplicationInfo, categoryId: savedCategory.uuid })
         .expect(409)
 
       expect(response.body).toHaveProperty('mentor')
