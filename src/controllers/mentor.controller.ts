a6fa091d08cd8df423f913547f8c15edb3d7a746
Madhawa Monarawila <70215958+Madhawa97@users.noreply.github.com>
2023-09-30 19:24:00 +0530

    Implement Get mentor by id endpoint (Public endpoint) (#65)

--- src/controllers/mentor.controller.ts
+++ src/controllers/mentor.controller.ts
@@ -1,5 +1,9 @@
 import type { Request, Response } from 'express'
-import { createMentor, updateAvailability } from '../services/mentor.service'
+import {
+  createMentor,
+  updateAvailability,
+  getMentor
+} from '../services/mentor.service'
 import type Profile from '../entities/profile.entity'
 
 export const mentorApplicationHandler = async (
@@ -35,6 +39,40 @@ export const mentorAvailabilityHandler = async (
     const { availability } = req.body
     const result = await updateAvailability(user, availability)
     res.status(result.statusCode).json(result.updatedMentorApplication)
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error('Error executing query', err)
+      res
+        .status(500)
+        .json({ error: 'Internal server error', message: err.message })
+    }
+  }
+}
+
+export const mentorDetailsHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const mentorId = req.params.mentorId
+    const { mentor, statusCode, message } = await getMentor(mentorId)
+
+    if (!mentor) {
+      res.status(statusCode).json({ error: message })
+    } else {
+      const mentorDetails = {
+        mentorId: mentor.uuid,
+        category: mentor.category.category,
+        profile: {
+          contact_email: mentor.profile.contact_email,
+          first_name: mentor.profile.first_name,
+          last_name: mentor.profile.last_name,
+          image_url: mentor.profile.image_url,
+          linkedin_url: mentor.profile.linkedin_url
+        }
+      }
+      res.status(statusCode).json({ ...mentorDetails })
+    }
   } catch (err) {
     if (err instanceof Error) {
       console.error('Error executing query', err)
