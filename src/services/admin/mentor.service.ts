ee7c77b8945f65f75cf30eff2127fb5ce4f1bbbc
Krishnadeva <krishnadeva@gl.block-stars.com>
2023-10-04 23:29:53 +0530

    Implement Get all mentors endpoint (Admin) #24

--- src/services/admin/mentor.service.ts
+++ src/services/admin/mentor.service.ts
@@ -1,6 +1,19 @@
 import { dataSource } from '../../configs/dbConfig'
 import Mentor from '../../entities/mentor.entity'
 import type { ApplicationStatus } from '../../enums'
+import type { AllMentorsArray, MentorInfo } from '../../types'
+
+const applicationFormat: MentorInfo = {
+  designation: '',
+  country: '',
+  areas_of_expertise: '',
+  expectations_from_mentees: '',
+  mentoring_philosophy: '',
+  commitment_to_program: false,
+  previous_experience_as_mentor: false,
+  reason_for_being_mentor: '',
+  cv_link: ''
+}
 
 export const updateMentorStatus = async (
   mentorId: string,
@@ -34,5 +47,87 @@ export const updateMentorStatus = async (
   } catch (err) {
     console.error('Error updating the mentor status', err)
     throw new Error('Error updating the mentor status')
+  }
+}
+
+export const getAllMentors = async (
+  status: ApplicationStatus
+): Promise<{
+  statusCode: number
+  mentors?: AllMentorsArray[]
+  message: string
+}> => {
+  try {
+    const mentorRepository = dataSource.getRepository(Mentor)
+
+    const mentorsList: Mentor[] = await mentorRepository.find({
+      where: { state: status },
+      select: [
+        'application',
+        'availability',
+        'state',
+        'created_at',
+        'updated_at'
+      ],
+      relations: ['profile', 'category']
+    })
+
+    const mentors: AllMentorsArray[] = mentorsList.map((mentor, i) => {
+      Object.entries(mentor.application).map((item) => {
+        switchQuestion(item, applicationFormat)
+      })
+      return {
+        ...mentor,
+        mentor_id: i + 1,
+        application: applicationFormat,
+        category: mentor.category.category,
+        state: mentor.state.toUpperCase()
+      }
+    })
+
+    if (!mentors) {
+      return {
+        statusCode: 404,
+        message: 'Mentors not found'
+      }
+    }
+
+    return {
+      statusCode: 200,
+      mentors,
+      message: 'All Mentors found'
+    }
+  } catch (err) {
+    console.error('Error updating the mentor status', err)
+    throw new Error('Error updating the mentor status')
+  }
+}
+
+function switchQuestion(item: any, format: MentorInfo): void {
+  switch (item[1].question.toLowerCase()) {
+    case 'what is your country?':
+      format.country = item[1].answers
+      break
+    case 'what is your expertise?':
+      format.areas_of_expertise = item[1].answers
+      break
+    case 'what is your mentoring startegy?':
+      format.mentoring_philosophy = item[1].answers
+      break
+    case 'what is your designation?':
+      format.designation = item[1].answers
+      break
+    case 'what is your reason for being mentor?':
+      format.reason_for_being_mentor = item[1].answers
+      break
+    case 'what is your expectations from mentees?':
+      format.expectations_from_mentees = item[1].answers
+      break
+    case 'do you have revious experience as mentor?':
+      format.previous_experience_as_mentor = item[1].answers
+      break
+    default:
+      // Handle other cases if needed
+      break
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import type Profile from './entities/profile.entity'
 interface Option {
   // todo: To be determined (Not final)
   answer: string
@@ -15,4 +16,25 @@ export interface MenteeApplication {
   answers: Question[]
   state: string
   mentor_id: bigint
+}
+
+export interface MentorInfo {
+  designation: string
+  country: string
+  areas_of_expertise: string
+  expectations_from_mentees: string
+  mentoring_philosophy: string
+  commitment_to_program: boolean
+  previous_experience_as_mentor: boolean
+  reason_for_being_mentor: string
+  cv_link: string
+}
+
+export interface AllMentorsArray {
+  mentor_id: number
+  state: string
+  category: string
+  application: MentorInfo
+  availability: boolean
+  profile: Profile
 }
