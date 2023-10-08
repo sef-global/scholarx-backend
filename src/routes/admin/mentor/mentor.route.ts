6764e219368cc7fe82f65c9ea94232c925d053a1
Krishnadeva Sathursan <67930416+sathudeva7@users.noreply.github.com>
2023-10-08 22:24:04 +0530

    Implement Get all mentors endpoint (Admin) #24 (#67)
    Co-authored-by: Krishnadeva <krishnadeva@gl.block-stars.com>

--- src/routes/admin/mentor/mentor.route.ts
+++ src/routes/admin/mentor/mentor.route.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { requireAuth } from '../../../controllers/auth.controller'
-import { mentorStatusHandler } from '../../../controllers/admin/mentor.controller'
+import {
+  getAllMentorsByStatus,
+  mentorStatusHandler
+} from '../../../controllers/admin/mentor.controller'
 
 const mentorRouter = express.Router()
 
 mentorRouter.put('/:mentorId/status', requireAuth, mentorStatusHandler)
+mentorRouter.get('/', requireAuth, getAllMentorsByStatus)
 
 export default mentorRouter
