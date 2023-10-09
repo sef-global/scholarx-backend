48962589ca06e56ea78d48a547b8f16072dcded0
Krishnadeva <krishnadeva@gl.block-stars.com>
2023-10-09 21:49:18 +0530

    Implemented Get all mentor emails endpoint (Admin) #43

--- src/controllers/admin/mentor.controller.ts
+++ src/controllers/admin/mentor.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express'
 import {
+  findAllMentorEmails,
   getAllMentors,
   updateMentorStatus
 } from '../../services/admin/mentor.service'
@@ -57,6 +58,36 @@ export const getAllMentorsByStatus = async (
       }
       const { mentors, statusCode, message } = await getAllMentors(status)
       res.status(statusCode).json({ mentors, message })
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error('Error executing query', err)
+      res
+        .status(500)
+        .json({ error: 'Internal server error', message: err.message })
+    }
+  }
+}
+
+export const getAllMentorEmails = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const user = req.user as Profile
+    const status: ApplicationStatus | undefined = req.query.status as
+      | ApplicationStatus
+      | undefined
+
+    if (user.type !== ProfileTypes.ADMIN) {
+      res.status(403).json({ message: 'Only Admins are allowed' })
+    } else {
+      if (status && !(status?.toUpperCase() in ApplicationStatus)) {
+        res.status(400).json({ message: 'Please provide a valid status' })
+        return
+      }
+      const { emails, statusCode, message } = await findAllMentorEmails(status)
+      res.status(statusCode).json({ emails, message })
     }
   } catch (err) {
     if (err instanceof Error) {
