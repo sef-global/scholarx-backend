68f183933988d498ccc4e62fdf01a86a5c4ec900
Krishnadeva <krishnadeva@gl.block-stars.com>
2023-10-11 00:48:03 +0530

    Revert "Refactored controller"
    This reverts commit e2416f6d4706062b18a38ff9c3bb103df031bb5c.

--- src/controllers/admin/mentor.controller.ts
+++ src/controllers/admin/mentor.controller.ts
@@ -78,7 +78,7 @@ export const getAllMentorsByStatus = async (
 export const getAllMentorEmails = async (
   req: Request,
   res: Response
-): Promise<ApiResponse<string[]>> => {
+): Promise<void> => {
   try {
     const user = req.user as Profile
     const status: ApplicationStatus | undefined = req.query.status as
@@ -86,22 +86,21 @@ export const getAllMentorEmails = async (
       | undefined
 
     if (user.type !== ProfileTypes.ADMIN) {
-      return res.status(403).json({ message: 'Only Admins are allowed' })
+      res.status(403).json({ message: 'Only Admins are allowed' })
+    } else {
+      if (status && !(status?.toUpperCase() in ApplicationStatus)) {
+        res.status(400).json({ message: 'Please provide a valid status' })
+        return
+      }
+      const { emails, statusCode, message } = await findAllMentorEmails(status)
+      res.status(statusCode).json({ emails, message })
     }
-
-    if (status && !(status?.toUpperCase() in ApplicationStatus)) {
-      return res.status(400).json({ message: 'Please provide a valid status' })
-    }
-
-    const { emails, statusCode, message } = await findAllMentorEmails(status)
-    return res.status(statusCode).json({ emails, message })
   } catch (err) {
     if (err instanceof Error) {
       console.error('Error executing query', err)
-      return res
+      res
         .status(500)
         .json({ error: 'Internal server error', message: err.message })
     }
-    throw err
   }
 }
