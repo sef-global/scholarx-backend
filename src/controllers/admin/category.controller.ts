8dd5fb16c6997d55dd24d3e6debe3e71118c6f45
Shrenik Deep <92735170+Shrenik0321@users.noreply.github.com>
2023-10-10 11:57:36 +0530

    Controller function refactor (#69)
    Co-authored-by: Yoshitha Rathnayake <yoshithasbrathnayake@gmail.com>

--- src/controllers/admin/category.controller.ts
+++ src/controllers/admin/category.controller.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from 'express'
 import type Profile from '../../entities/profile.entity'
 import { ProfileTypes } from '../../enums'
+import type Category from '../../entities/category.entity'
+import type { ApiResponse } from '../../types'
 import {
   changeCategory,
   createCategory
@@ -9,47 +11,43 @@ import {
 export const addCategory = async (
   req: Request,
   res: Response
-): Promise<void> => {
+): Promise<ApiResponse<Category>> => {
   try {
     const user = req.user as Profile
     const { categoryName } = req.body
 
     if (user.type !== ProfileTypes.ADMIN) {
-      res.status(403).json({ message: 'Only Admins are allowed' })
-    } else {
-      const { category, statusCode, message } = await createCategory(
-        categoryName
-      )
-
-      res.status(statusCode).json({ category, message })
+      return res.status(403).json({ message: 'Only Admins are allowed' })
     }
+
+    const { category, statusCode, message } = await createCategory(categoryName)
+    return res.status(statusCode).json({ category, message })
   } catch (err) {
     console.error('Error executing query', err)
-    res.status(500).json({ error: err })
+    return res.status(500).json({ error: err })
   }
 }
 
 export const updateCategory = async (
   req: Request,
   res: Response
-): Promise<void> => {
+): Promise<ApiResponse<Category>> => {
   try {
     const user = req.user as Profile
     const { categoryName } = req.body
     const { categoryId } = req.params
 
     if (user.type !== ProfileTypes.ADMIN) {
-      res.status(403).json({ message: 'Only Admins are allowed' })
-    } else {
-      const { category, statusCode, message } = await changeCategory(
-        categoryId,
-        categoryName
-      )
-
-      res.status(statusCode).json({ category, message })
+      return res.status(403).json({ message: 'Only Admins are allowed' })
     }
+
+    const { category, statusCode, message } = await changeCategory(
+      categoryId,
+      categoryName
+    )
+    return res.status(statusCode).json({ category, message })
   } catch (err) {
     console.error('Error executing query', err)
-    res.status(500).json({ error: err })
+    return res.status(500).json({ error: err })
   }
 }
