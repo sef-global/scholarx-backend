255edff7bc7b97b89bcceb2a20da996c5eb9a991
Krishnadeva Sathursan <67930416+sathudeva7@users.noreply.github.com>
2023-10-15 11:10:51 +0530

    Implement Add platform details endpoint (Admin) #37 (#74)
    Co-authored-by: Krishnadeva <krishnadeva@gl.block-stars.com>

---
+++ src/controllers/admin/platform.controller.ts
@@ -0,0 +1,47 @@
+import type { Request, Response } from 'express'
+import type { ApiResponse } from '../../types'
+import type Platform from '../../entities/platform.entity'
+import type Profile from '../../entities/profile.entity'
+import { ProfileTypes } from '../../enums'
+import {
+  createPlatform,
+  getPlatformDetails
+} from '../../services/admin/platform.service'
+
+export const addPlatform = async (
+  req: Request,
+  res: Response
+): Promise<ApiResponse<Platform>> => {
+  try {
+    const user = req.user as Profile
+
+    if (user.type !== ProfileTypes.ADMIN) {
+      return res.status(403).json({ message: 'Only Admins are allowed' })
+    }
+
+    const { platform, statusCode, message } = await createPlatform(req.body)
+    return res.status(statusCode).json({ platform, message })
+  } catch (err) {
+    console.error('Error executing query', err)
+    return res.status(500).json({ error: err })
+  }
+}
+
+export const getPlatform = async (
+  req: Request,
+  res: Response
+): Promise<ApiResponse<Platform>> => {
+  try {
+    const user = req.user as Profile
+
+    if (user.type !== ProfileTypes.ADMIN) {
+      return res.status(403).json({ message: 'Only Admins are allowed' })
+    }
+
+    const { platform, statusCode, message } = await getPlatformDetails()
+    return res.status(statusCode).json({ platform, message })
+  } catch (err) {
+    console.error('Error executing query', err)
+    return res.status(500).json({ error: err })
+  }
+}
