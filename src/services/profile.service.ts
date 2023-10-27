08318328cbbe64f33195f63cfc72620c35a2915a
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-10-28 02:43:04 +0530

    Implement get my mentor applications endpoint

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,4 +1,5 @@
 import { dataSource } from '../configs/dbConfig'
+import Mentor from '../entities/mentor.entity'
 import Profile from '../entities/profile.entity'
 
 export const updateProfile = async (
@@ -55,4 +56,48 @@ export const deleteProfile = async (userId: string): Promise<void> => {
     .from(Profile)
     .where('uuid = :uuid', { uuid: userId })
     .execute()
+}
+
+export const getAllMentorApplications = async (
+  user: Profile
+): Promise<{
+  statusCode: number
+  mentorApplications?: Mentor[] | null | undefined
+  message: string
+}> => {
+  try {
+    const mentorRepository = dataSource.getRepository(Mentor)
+
+    const existingMentorApplications = await mentorRepository
+      .createQueryBuilder('mentor')
+      .select([
+        'mentor.state AS state',
+        'mentor.availability AS availability',
+        'mentor.uuid AS uuid',
+        'mentor.created_at AS created_at',
+        'mentor.updated_at AS updated_at',
+        'mentor.application AS application',
+        'category.category AS category',
+        'category.uuid AS category_uuid'
+      ])
+      .leftJoin('mentor.category', 'category')
+      .where('mentor.profile.uuid = :uuid', { uuid: user.uuid })
+      .getRawMany()
+
+    if (existingMentorApplications.length === 0) {
+      return {
+        statusCode: 200,
+        message: 'No mentor applications found for the user'
+      }
+    }
+
+    return {
+      statusCode: 200,
+      mentorApplications: existingMentorApplications,
+      message: 'Mentor applications found'
+    }
+  } catch (error) {
+    console.error('Error executing query', error)
+    return { statusCode: 500, message: 'Internal server error' }
+  }
 }
