ea9824f81884d49dae2f23a9eb2342adb96ed2bb
Kumuditha Karunarathna <kumudithaudaya@gmail.com>
2024-04-28 18:23:10 +0530

    Implement approve/reject mentee endpoint for admins and mentors (#108)
    Co-authored-by: kumuditha-udayanga <sumedhachathuranga@gmail.com>

--- src/controllers/mentee.controller.ts
+++ src/controllers/mentee.controller.ts
@@ -2,7 +2,8 @@ import type { Request, Response } from 'express'
 import { type ApiResponse } from '../types'
 import type Mentee from '../entities/mentee.entity'
 import type Profile from '../entities/profile.entity'
-import { addMentee } from '../services/admin/mentee.service'
+import { addMentee, updateStatus } from '../services/admin/mentee.service'
+import { ProfileTypes } from '../enums'
 
 export const menteeApplicationHandler = async (
   req: Request,
@@ -25,6 +26,32 @@ export const menteeApplicationHandler = async (
         .json({ error: 'Internal server error', message: err.message })
     }
 
+    throw err
+  }
+}
+
+export const updateMenteeStatus = async (
+  req: Request,
+  res: Response
+): Promise<ApiResponse<Mentee>> => {
+  try {
+    const user = req.user as Profile
+    const { state } = req.body
+    const { menteeId } = req.params
+
+    if (user.type !== ProfileTypes.ADMIN) {
+      return res.status(403).json({ message: 'Only Admins are allowed' })
+    }
+
+    const { statusCode, message } = await updateStatus(menteeId, state)
+    return res.status(statusCode).json({ message })
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error('Error executing query', err)
+      return res
+        .status(500)
+        .json({ error: 'Internal server error', message: err.message })
+    }
     throw err
   }
 }
