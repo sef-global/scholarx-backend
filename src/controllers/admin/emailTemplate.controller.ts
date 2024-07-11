d9b37cad09346048828314999a0e4dbf6d2171ab
Dileepa Mabulage <Dileepa.Mabulage@inivosglobal.com>
2024-07-11 19:39:25 +0530

    add delete email template for admin and unit tests

--- src/controllers/admin/emailTemplate.controller.ts
+++ src/controllers/admin/emailTemplate.controller.ts
@@ -1,12 +1,13 @@
 import type { Request, Response } from 'express'
-import type { ApiResponse } from '../../types'
+import type EmailTemplate from '../../entities/emailTemplate.entity'
 import type Profile from '../../entities/profile.entity'
 import { ProfileTypes } from '../../enums'
 import {
   createEmailTemplate,
+  deleteEmailTemplateById,
   getEmailTemplateById
 } from '../../services/admin/emailTemplate.service'
-import type EmailTemplate from '../../entities/emailTemplate.entity'
+import type { ApiResponse } from '../../types'
 
 export const addEmailTemplate = async (
   req: Request,
@@ -52,6 +53,26 @@ export const getEmailTemplate = async (
       templateId
     )
     return res.status(statusCode).json({ emailTemplate, message })
+  } catch (err) {
+    console.error('Error executing query', err)
+    return res.status(500).json({ error: err })
+  }
+}
+
+export const deleteEmailTemplate = async (
+  req: Request,
+  res: Response
+): Promise<ApiResponse<EmailTemplate>> => {
+  try {
+    const user = req.user as Profile
+    const templateId = req.params.templateId
+
+    if (user.type !== ProfileTypes.ADMIN) {
+      return res.status(403).json({ message: 'Only Admins are allowed' })
+    }
+
+    const { statusCode, message } = await deleteEmailTemplateById(templateId)
+    return res.status(statusCode).json({ message })
   } catch (err) {
     console.error('Error executing query', err)
     return res.status(500).json({ error: err })
--- src/routes/admin/email/emailTemplate.route.ts
+++ src/routes/admin/email/emailTemplate.route.ts
@@ -1,13 +1,15 @@
 import express from 'express'
-import { requireAuth } from '../../../controllers/auth.controller'
 import {
   addEmailTemplate,
+  deleteEmailTemplate,
   getEmailTemplate
 } from '../../../controllers/admin/emailTemplate.controller'
+import { requireAuth } from '../../../controllers/auth.controller'
 
 const emailTemplateRouter = express.Router()
 
 emailTemplateRouter.post('/', requireAuth, addEmailTemplate)
 emailTemplateRouter.get('/:templateId', requireAuth, getEmailTemplate)
+emailTemplateRouter.delete('/:templateId', requireAuth, deleteEmailTemplate)
 
 export default emailTemplateRouter
