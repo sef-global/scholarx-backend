c6cdc90be901a61b1c4ab64bb589445a88eacae8
Dileepa Mabulage <Dileepa.Mabulage@inivosglobal.com>
2024-07-14 21:24:40 +0530

    add mentee route and status validation, unit test were previously in the code

--- src/routes/admin/mentee/mentee.route.ts
+++ src/routes/admin/mentee/mentee.route.ts
@@ -9,8 +9,10 @@ import {
 
 const menteeRouter = express.Router()
 
-menteeRouter.get('/emails/', requireAuth, getAllMenteeEmails)
-menteeRouter.get('/applications', requireAuth, getMentees)
+menteeRouter.get('/', requireAuth, getMentees)
+menteeRouter.get('/emails', requireAuth, getAllMenteeEmails)
+// TODO: Remove this route
+// menteeRouter.get('/applications', requireAuth, getMentees)
 menteeRouter.get('/:menteeId', requireAuth, getMenteeDetails)
 menteeRouter.put('/:menteeId/state', requireAuth, updateMenteeStatus)
 
