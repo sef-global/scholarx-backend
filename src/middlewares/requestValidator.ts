8f57db256f4ed778733d4d7bf0da9e6c6bb1d090
Dileepa Mabulage <Dileepa.Mabulage@inivosglobal.com>
2024-08-01 11:48:34 +0530

    implement request validator

---
+++ src/middlewares/requestValidator.ts
@@ -0,0 +1,21 @@
+import { ZodError, ZodSchema } from 'zod'
+
+export const requestValidator = (schema: ZodSchema<any, any, any>) => {
+  return (req: any, res: any, next: any) => {
+    try {
+      schema.parse(req.body)
+      next()
+    } catch (err) {
+      console.log(err)
+      if (err instanceof ZodError) {
+        const errorMessages = err.errors.map((issue) => ({
+          message: `${issue.path.join('.')} is ${issue.message}`
+        }))
+        return res
+          .status(400)
+          .json({ error: 'Invalid data', details: errorMessages })
+      }
+      return res.status(500).json({ error: 'Internal server error' })
+    }
+  }
+}
