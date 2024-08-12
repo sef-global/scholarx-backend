40b0808c33b6a6fa8135b4832501be2cf8b4f0de
Dillepa Mabulage <dileepainivos@gmail.com>
2024-08-12 21:13:01 +0530

    add pagination for category controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,16 +1,26 @@
 import type { Request, Response } from 'express'
-import { getAllCategories } from '../services/category.service'
-import type { ApiResponse } from '../types'
 import type Category from '../entities/category.entity'
+import { getAllCategories } from '../services/category.service'
+import { type PaginatedApiResponse } from '../types'
 
 export const getCategories = async (
   req: Request,
   res: Response
-): Promise<ApiResponse<Category>> => {
+): Promise<Response<PaginatedApiResponse<Category>>> => {
   try {
-    const { statusCode, categories, message } = await getAllCategories()
+    const pageNumber = parseInt(req.query.pageNumber as string) || 1
+    const pageSize = parseInt(req.query.pageSize as string) || 10
 
-    return res.status(statusCode).json({ categories, message })
+    const { statusCode, items, totalItemCount, message } =
+      await getAllCategories(pageNumber, pageSize)
+
+    return res.status(statusCode).json({
+      pageNumber,
+      pageSize,
+      totalItemCount,
+      items,
+      message
+    })
   } catch (err) {
     if (err instanceof Error) {
       console.error('Error executing query', err)
